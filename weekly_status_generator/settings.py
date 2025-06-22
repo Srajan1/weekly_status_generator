58c39ea646aebeee1f87e646503d5c70cb872228
Srajan1 <srakishor.g@gmail.com>
2025-06-22 14:55:39 +0530

    Added dotenv instead of using env variables inside docker compose, reduces parity between local setup and docker

--- weekly_status_generator/settings.py
+++ weekly_status_generator/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,6 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
+print(ALLOWED_HOSTS)
 
 # Application definition
 
